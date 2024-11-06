d20ffae7c68da45dfc4c5172638bbdeb4ad1f2e3
Xusanboy <xusanboy@gmail.com>
2024-11-06 16:34:27 +0500

    Button changes

--- src/main/java/uz/tenzorsoft/scaleapplication/ui/MainController.java
+++ src/main/java/uz/tenzorsoft/scaleapplication/ui/MainController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
@@ -39,6 +40,35 @@ public class MainController {
 
     @FXML
     private TextField phoneNumber;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button3;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private Button button5;
+
+    @FXML
+    public void initialize() {
+        setupButtonPressEffect(button1, "#b2f5a9", "#a8e69f");
+        setupButtonPressEffect(button2, "#b2f5a9", "#a8e69f");
+        setupButtonPressEffect(button3, "#ebed72", "#e3e360");
+        setupButtonPressEffect(button4, "#f56e6e", "#e46464");
+        setupButtonPressEffect(button5, "#f56e6e", "#e46464");
+    }
+
+    private void setupButtonPressEffect(Button button, String normalColor, String pressedColor) {
+        button.setOnMousePressed(event -> button.setStyle("-fx-background-color: " + pressedColor + "; -fx-border-color: black; -fx-border-radius: 7; -fx-background-radius: 10; -fx-border-width: 0.8;"));
+        button.setOnMouseReleased(event -> button.setStyle("-fx-background-color: " + normalColor + "; -fx-border-color: black; -fx-border-radius: 7; -fx-background-radius: 10; -fx-border-width: 0.8;"));
+    }
 
     public void load() {
         try {
