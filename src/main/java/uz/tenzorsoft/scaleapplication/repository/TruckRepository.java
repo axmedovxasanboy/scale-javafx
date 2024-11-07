dae42b93d825171af63d3f87f3a29d166392fcea
Xusanboy <xusanboy@gmail.com>
2024-11-07 09:56:53 +0500

    Bugs fixed

--- src/main/java/uz/tenzorsoft/scaleapplication/repository/TruckRepository.java
+++ src/main/java/uz/tenzorsoft/scaleapplication/repository/TruckRepository.java
@@ -16,16 +16,6 @@ import java.util.Optional;
 @Repository
 public interface TruckRepository extends JpaRepository<TruckEntity, Long> {
 
-    @Query("SELECT t FROM trucks t WHERE t.truckNumber = :truckNumber AND t.truckAction.action = :entranceAction")
-    TruckEntity findEnteredTruckByTruckNumberAndNotExited(@Param("truckNumber") String truckNumber, @Param("entranceAction") TruckAction entranceAction);
-
-    // Query to find a truck by truck number and truck action
-    @Query("SELECT t FROM trucks t JOIN t.truckAction ta WHERE t.truckNumber = :truckNumber AND ta.action = :truckAction")
-    TruckEntity findByTruckNumberAndAction(@Param("truckNumber") String truckNumber, @Param("truckAction") TruckAction truckAction);
-
-    @Query("SELECT t FROM trucks t WHERE t.truckAction.action = :action")
-    List<TruckEntity> findEnteredTrucks(TruckAction action);
-
     List<TruckEntity> findByIsSent(boolean isSent);
 
     Optional<TruckEntity> findTopByOrderByIdDesc();
