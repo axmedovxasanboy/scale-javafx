ecc5c1ce922cef2d3ed96a791130e4f9f6fd315e
Ilyosbek <imamashukurov@gmail.com>
2024-11-15 18:35:47 +0500

    websocket processda 2

---
+++ src/main/java/uz/tenzorsoft/scaleapplication/websocket/CommandsDto.java
@@ -0,0 +1,26 @@
+package uz.tenzorsoft.scaleapplication.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommandsDto implements Serializable {
+    private Integer scaleId;
+    private Boolean openGate1;
+    private Boolean closeGate1;
+    private Boolean weighing;
+    private Boolean openGate2;
+    private Boolean closeGate2;
+
+    @Override
+    public String toString() {
+        return "CommandsDto(scaleId=" + scaleId + ", openGate1=" + openGate1 + ", closeGate1=" + closeGate1 + ", weighing=" + weighing + ", openGate2=" + openGate2 + ", closeGate2=" + closeGate2 + ")";
+    }
+}
--- src/main/java/uz/tenzorsoft/scaleapplication/websocket/ComputerClient.java
+++ src/main/java/uz/tenzorsoft/scaleapplication/websocket/ComputerClient.java
@@ -1,6 +1,5 @@
 package uz.tenzorsoft.scaleapplication.websocket;
 
-import com.ghgande.j2mod.modbus.ModbusException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
@@ -13,13 +12,12 @@ import org.slf4j.LoggerFactory;
 import uz.tenzorsoft.scaleapplication.service.ControllerService;
 
 import javax.annotation.PostConstruct;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @Service
 public class ComputerClient {
 
-    private static final String WEBSOCKET_URL = "ws://scale.mycoal.uz/ws";
+    private static final String WEBSOCKET_URL = "ws://192.168.68.136:9090/ws"; // Serverning IP manzili va porti
     private static final Logger logger = LoggerFactory.getLogger(ComputerClient.class);
 
     private final ControllerService controllerService;
@@ -33,46 +31,23 @@ public class ComputerClient {
     public void connectToWebSocket() {
         WebSocketStompClient stompClient = new WebSocketStompClient(new StandardWebSocketClient());
         StompSessionHandlerAdapter sessionHandler = new StompSessionHandlerAdapter() {
+
             @Override
             public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
-                session.subscribe("/topic/door-control", this);
+                session.subscribe("/topic/commands", this);
                 logger.info("WebSocket ulanish muvaffaqiyatli amalga oshirildi!");
             }
 
             @Override
             public void handleFrame(StompHeaders headers, Object payload) {
-                if (payload instanceof Map<?, ?>) {
-                    Map<String, Boolean> doorStatus = (Map<String, Boolean>) payload;
-
-                    try {
-                        if (doorStatus.getOrDefault("openGate1", false)) {
-                            controllerService.openGate1();
-                            logger.info("Kirish darvozasi 1 ochildi");
-                        } else if (doorStatus.getOrDefault("closeGate1", false)) {
-                            controllerService.closeGate1();
-                            logger.info("Kirish darvozasi 1 yopildi");
-                        } else if (doorStatus.getOrDefault("openGate2", false)) {
-                            controllerService.openGate2();
-                            logger.info("Chiqish darvozasi 2 ochildi");
-                        } else if (doorStatus.getOrDefault("closeGate2", false)) {
-                            controllerService.closeGate2();
-                            logger.info("Chiqish darvozasi 2 yopildi");
-                        } else {
-                            logger.warn("Noma'lum buyruq qabul qilindi!");
-                        }
-                    } catch (ModbusException e) {
-                        logger.error("Modbus xatolik yuz berdi: {}", e.getMessage());
-                    }
-                } else {
-                    logger.error("Noma'lum payload turi qabul qilindi: {}", payload.getClass().getName());
-                }
+                logger.info("Xabar kelib tushdi: {}", payload);
             }
         };
 
         try {
             stompClient.connect(WEBSOCKET_URL, sessionHandler).get();
         } catch (InterruptedException | ExecutionException e) {
-            logger.error("WebSocket ulanishi amalga oshirilishda xatolik yuz berdi: {}", e.getMessage());
+            logger.error("WebSocket ulanishi amalga oshirilishda xatolik: {}", e.getMessage());
         }
     }
 }
